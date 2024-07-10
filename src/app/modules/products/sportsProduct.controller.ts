b56f12b4d03c8a9b30f527122831ff6dcfa55fac
AALabonya <1aklimakter@gmail.com>
2024-07-09 19:22:08 -0700

    added product controller

---
+++ src/app/modules/products/sportsProduct.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import { ProductServices } from './sportsProduct.service';
+
+const createProduct = async (req: Request, res: Response) => {
+  console.log(req.body);
+
+  const result = await ProductServices.createProductIntoDB(req.body);
+  res.json({
+    success: true,
+    message: 'Product created successfully',
+    data: result,
+  });
+};
+
+const getAllProduct = async (req: Request, res: Response) => {
+  const result = await ProductServices.getAllProductFromDB();
+  res.json({
+    success: true,
+    message: 'Successfully retrieve All Products',
+    data: result,
+  });
+};
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ProductServices.getSingleProductFromDB(id);
+  res.json({
+    success: true,
+    message: 'Successfully Retrieve A Products',
+    data: result,
+  });
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ProductServices.updateProductIntoDB(id, req.body);
+  res.json({
+    success: true,
+    message: 'Successfully Update A Products',
+    data: result,
+  });
+};
+
+export const ProductController = {
+  createProduct,
+  getAllProduct,
+  getSingleProduct,
+  updateProduct,
+};
